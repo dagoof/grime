6a244b9082ce4b1b26da1ecaaded937857a9251c
Peter DeGroff <peter@roamz.com>
2014-05-06 13:14:40 +1000

    blocking ticker struct was not needed

--- blocking_ticker.go
+++ blocking_ticker.go
@@ -4,30 +4,16 @@ import (
 	"time"
 )
 
-// BlockingTicker holds a channel that delivers ticks according to a
-// Stepper. All steps are read and sent through the contained channel.
-type BlockingTicker struct {
-	Stepper
-	C    chan time.Time
-	stop chan struct{}
-}
-
-// Stop the ticker. No more ticks will be sent after stop has been called, but
-// it does not close the tick channel.
-func (t *BlockingTicker) Stop() {
-	close(t.stop)
-}
-
 // NewBlockingTicker creates a Ticker containing a channel that will send the
 // ticks according to a Stepper. Steps will never be discarded.
-func NewBlockingTicker(s Stepper) *BlockingTicker {
-	ticker := &BlockingTicker{
+func NewBlockingTicker(s Stepper) *Ticker {
+	ticker := &Ticker{
 		s,
-		make(chan time.Time),
+		make(chan time.Time, 1),
 		make(chan struct{}),
 	}
 
-	go func(t *BlockingTicker) {
+	go func(t *Ticker) {
 		tick := t.Step()
 
 		for {
